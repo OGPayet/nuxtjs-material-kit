5201432a3ab9249642f7db46e657701ee75997cf
Pierre Payet <pierre.payet@outlook.com>
2021-04-13 22:09:38 +0200

    NuxtJS Material Kit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="{ 'nav-open': $store.state.navbar }">
+    <MainNavbar />
+    <nuxt />
+    <MainFooter />
+  </div>
+</template>
+
+<script>
+import MainNavbar from '../components/MainNavbar.vue'
+import MainFooter from '../components/MainFooter.vue'
+
+export default {
+  components: {
+    MainNavbar,
+    MainFooter,
+  },
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,113 @@
+const pkg = require('./package')
+
+module.exports = {
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: pkg.name,
+    meta: [
+      {
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: pkg.description
+      }
+    ],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      },
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:100:300,400,500,700,900|Material+Icons'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://use.fontawesome.com/releases/v5.0.8/css/all.css'
+      }
+    ]
+  },
+
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#fff'
+  },
+
+  /*
+   ** Global CSS
+   */
+  css: [
+    'vue-material/dist/vue-material.min.css',
+    '~/assets/scss/material-kit.scss',
+    '~/assets/demo.css'
+  ],
+
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [
+    '~/plugins/vue-material',
+    '~/plugins/vue-lazyload',
+    {
+      src: '~plugins/vue-carousel',
+      ssr: false
+    },
+    '~/plugins/globals'
+  ],
+
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios'
+  ],
+
+  buildModules: [
+    '@nuxtjs/eslint-module'
+  ],
+
+  /*
+   ** Axios module configuration
+   */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  eslint: {
+    fix: true
+  },
+
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
